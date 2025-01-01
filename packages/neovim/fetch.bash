e4fbb6149f113659638692cc41086d88709d5e61
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2025-01-02 00:13:33 +0200

    Install curl in build script

--- packages/neovim/fetch.bash
+++ packages/neovim/fetch.bash
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
-workdir="${MINIT_BUILD_WORKDIR:-"/tmp/build"}"
+workdir="${MINIT_BUILD_WORKDIR:-"/app/build"}"
 version="${MINIT_BUILD_VERSION:-"v0.10.3"}"
 arch="${MINIT_BUILD_ARCH:-"$(arch)"}"
 os="${MINIT_BUILD_OS:-"macos"}"
 
+echo "$workdir"
+echo "$version"
+echo "$arch"
+echo "$os"
+
 mkdir -p "$workdir"
 cd "$workdir" || exit 1
 
+apt update && apt install --yes curl
+
 file_name="nvim-${os}-${arch}.tar.gz"
-curl --remote-name "https://github.com/neovim/neovim/releases/download/${version}/${file_name}"
+file_url="https://github.com/neovim/neovim/releases/download/${version}/${file_name}"
+curl --remote-name "$file_url"
 
 # TODO: consider moving the sha256sums to repo
-curl --remote-name "https://github.com/neovim/neovim/releases/download/${version}/${file_name}.sha256sum"
+curl --remote-name "${file_url}.sha256sum"
 sha256sum --check --status "${file_name}.sha256sum" || exit 1
