3fdd62a85c154450f4f767dbb9acbfaf770ccb44
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2025-01-05 13:44:16 +0200

    Remove untar and move final artifact to out dir

--- packages/neovim/darwin/arm64/fetch.bash
+++ packages/neovim/darwin/arm64/fetch.bash
@@ -3,16 +3,19 @@
 set -euxo pipefail
 
 workdir="${MINIT_BUILD_WORKDIR:-"/app/build"}"
+outdir="${MINIT_BUILD_OUTDIR:-"/app/final"}"
 version="${MINIT_BUILD_VERSION:-"v0.10.3"}"
 arch="${MINIT_BUILD_ARCH:-"arm64"}"
 os="${MINIT_BUILD_OS:-"macos"}"
 
 echo "$workdir"
+echo "$outdir"
 echo "$version"
 echo "$arch"
 echo "$os"
 
 mkdir -p "$workdir"
+mkdir -p "$outdir"
 cd "$workdir"
 
 apt update && apt install --yes curl
@@ -29,5 +32,4 @@ cat "${file_name}.sha256sum"
 
 sha256sum --check --status "${file_name}.sha256sum"
 
-tar xzvf "$file_name"
-stat "./nvim-${os}-${arch}/bin/nvim"
+mv "./${file_name}" "${outdir}/${file_name}"
